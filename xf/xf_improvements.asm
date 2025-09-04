cf7bddd3bcd4d7c6852ae5462732be21358a91a1
H-A-M-G-E-R <hamgerham@proton.me>
2025-09-03 22:54:28 -0500

    Add ips file for xf improvements

--- xf/xf_improvements.asm
+++ xf/xf_improvements.asm
@@ -1,5 +1,5 @@
 ; Patches for Meta's X-Fusion hack (https://metroidconstruction.com/hack.php?id=837) that makes it feel a bit more like Fusion.
-; Patch it on xf_v1.2_gba-eng-fre-jap-ger.
+; Patch it (or xf_improvements.ips) on xf_v1.2_gba-eng-fre-jap-ger.
 lorom
 
 org $90FF6D ; little bit of freespace :)
